7bb4daef23011096fb8d646d28195475e251c729
Vlad Vikhrov <vikhrov.vs@phystech.edu>
2023-05-03 00:49:55 +0300

    full selection

--- bot.py
+++ bot.py
@@ -5,12 +5,14 @@ from datetime import datetime
 from aiogram import Bot, types
 from aiogram.dispatcher import Dispatcher
 from aiogram.dispatcher import FSMContext
+from aiogram.dispatcher.filters import Text
 from aiogram.utils import executor
 import aiogram.utils.markdown as md
 from aiogram.types import ParseMode, CallbackQuery
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 
 from aiogram_calendar import simple_cal_callback, SimpleCalendar, dialog_cal_callback, DialogCalendar
+from aiogram_timepicker.panel import FullTimePicker, full_timep_callback
 
 
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
@@ -41,8 +43,8 @@ async def start(message: types.Message):
 class Event(StatesGroup):
     name = State()  # Will be represented in storage as 'Form:name'
     date = State()
+    time = State()
     period = State()
-
 
 
 @dp.message_handler(commands=["event"])
@@ -50,7 +52,20 @@ async def event(message: types.Message):
     await Event.name.set()
     await message.reply("Hi! Please, enter your event description")
 
-#TODO: cancel
+
+@dp.message_handler(state='*', commands='cancel')
+@dp.message_handler(Text(equals='cancel', ignore_case=True), state='*')
+async def cancel_handler(message: types.Message, state: FSMContext):
+    """
+    Allow user to cancel any action
+    """
+    current_state = await state.get_state()
+    if current_state is None:
+        return
+
+    # logging.info('Cancelling state %r', current_state)
+    await state.finish()
+    await message.answer('Event creation cancelled', reply_markup=types.ReplyKeyboardRemove())
 
 
 @dp.message_handler(state=Event.name)
@@ -61,16 +76,31 @@ async def process_name(message: types.Message, state: FSMContext):
     await message.reply("Please, enter an event date", reply_markup=await SimpleCalendar().start_calendar())
 
 
-# simple calendar usage
 @dp.callback_query_handler(simple_cal_callback.filter(), state=Event.date)
 async def process_simple_calendar(callback_query: CallbackQuery, callback_data: dict, state: FSMContext):
     selected, date = await SimpleCalendar().process_selection(callback_query, callback_data)
     if selected:
         await Event.next()
         await state.update_data(date=date)
-        # await callback_query.message.answer(
-        #     f'You selected {date.strftime("%d/%m/%Y")}'
-        # )
+        await callback_query.message.answer(
+            f'You selected {date.strftime("%d/%m/%Y")}'
+        )
+
+        await callback_query.message.answer("What is the time of your event?",
+                                            reply_markup=await FullTimePicker().start_picker())
+
+
+@dp.callback_query_handler(full_timep_callback.filter(), state=Event.time)
+async def process_name(callback_query: CallbackQuery, callback_data: dict, state: FSMContext):
+    r = await FullTimePicker().process_selection(callback_query, callback_data)
+    selected, selected_time = r.selected, r.time
+    if selected:
+        await Event.next()
+        await state.update_data(time=selected_time)
+        await callback_query.message.answer(
+            f'You selected {selected_time.strftime("%H:%M:%S")}'
+        )
+        await callback_query.message.delete_reply_markup()
 
         markup = types.ReplyKeyboardMarkup(resize_keyboard=True, selective=True)
         markup.add("Every day", "Every week", "Every 10s")
@@ -92,6 +122,7 @@ async def process_period(message: types.message, state: FSMContext):
             md.text(
                 md.text("Event description: ", md.bold(data['name'])),
                 md.text("Date: ", md.code(data['date'])),
+                md.text("Time: ", md.code(data['time'])),
                 md.text("Period: ", data['period']),
                 sep='\n',
             ),
